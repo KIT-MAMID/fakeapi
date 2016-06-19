7905243f16737ab5efbf5835c1d10ec8611c9244
Anton Schirg <anton.schirg@posteo.de>
2016-06-19 17:20:00 +0200

    More slave properties

--- apiobjects/slaves.go
+++ apiobjects/slaves.go
@@ -8,15 +8,22 @@ import (
 )
 
 type Slave struct {
-	Id uint			`json:"id"`
-	Hostname string		`json:"hostname"`
-	Port uint		`json:"slave_port"`
+	Id uint			   `json:"id"`
+	Hostname string		   `json:"hostname"`
+	Port uint		   `json:"slave_port"`
+	MongodPortRangeBegin uint  `json:"mongod_port_range_begin"` //inclusive
+	MongodPortRangeEnd uint    `json:"mongod_port_range_end"`   //exclusive
+	PersistantStorage bool     `json:"persistant_storage"`
+	RootDataDirectory string   `json:"root_data_directory"`
+	State string               `json:"state"`
 }
 
 func getSlaves() []Slave {
 	return []Slave{
-		Slave{Hostname: "mksuns31", Port: 1912},
-		Slave{Hostname: "mksuns32", Port: 1912},
+		Slave{Id: 0, Hostname: "mksuns31", Port: 1912, MongodPortRangeBegin: 20000, MongodPortRangeEnd:20100, PersistantStorage:true, RootDataDirectory:"/home/mongo/data", State: "active"},
+		Slave{Id: 1, Hostname: "mksuns32", Port: 1912, MongodPortRangeBegin: 20000, MongodPortRangeEnd:20001, PersistantStorage:false, RootDataDirectory:"/home/mongo/data", State: "active"},
+		Slave{Id: 2, Hostname: "mksuns33", Port: 1912, MongodPortRangeBegin: 20000, MongodPortRangeEnd:20001, PersistantStorage:false, RootDataDirectory:"/home/mongo/data", State: "active"},
+		Slave{Id: 3, Hostname: "mksuns34", Port: 1912, MongodPortRangeBegin: 20000, MongodPortRangeEnd:20001, PersistantStorage:false, RootDataDirectory:"/home/mongo/data", State: "active"},
 	}
 }
 
