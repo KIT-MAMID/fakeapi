885882e1ba14a89122d8581bf996e5e863dd0f58
Janis Streib <janis@dogcraft.de>
2016-06-19 17:46:33 +0200

    ADD: static file serve

--- main/main.go
+++ main/main.go
@@ -2,26 +2,19 @@ package main
 
 import (
 	"net/http"
-	"fmt"
-	"html"
 	"log"
-
 	"github.com/gorilla/mux"
 	"github.com/KIT-MAMID/fakeapi/apiobjects"
 )
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	
-	router.HandleFunc("/", Index)
+
+	router.Handle("/", http.FileServer(http.Dir("./static/")))
 	router.Methods("GET").Path("/api/slaves").Name("SlaveIndex").HandlerFunc(apiobjects.SlaveIndex)
 	router.Methods("GET").Path("/api/slave/{slaveId}").Name("SlaveById").HandlerFunc(apiobjects.SlaveById)
 	router.Methods("GET").Path("/api/replicasets").Name("ReplicaSetIndex").HandlerFunc(apiobjects.ReplicaSetIndex)
 	router.Methods("GET").Path("/api/riskgroups").Name("RiskGroupIndex").HandlerFunc(apiobjects.RiskGroupIndex)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
 }
